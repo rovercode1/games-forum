be51e0f10dc40408a1a7acad5ff5ec711aaa41f9
Hayden <haydenworden@outlook.com>
2023-02-20 14:03:12 +0000

    Tests suite created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,45 @@ beforeEach(() => seed(data));
 
 afterAll(() => connection.end());
 describe('api',()=>{
-  it('', ()=>{
-    
+  describe('/api/reviews', ()=>{
+    it('200 GET - responds array of review objects, including the correct properties ',()=>{
+      return request(app)
+      .get('/api/reviews')
+      .expect(200)
+      .then(( {body} )=>{
+        const reviews = body
+        expect(Array.isArray(reviews)).toBe(true)
+
+        reviews.forEach((review)=>{
+          expect(
+            review.hasOwnProperty("owner", expect.any(Number))
+          ).toBe(true);
+          expect(
+            review.hasOwnProperty("title", expect.any(Number))
+          ).toBe(true);
+          expect(
+            review.hasOwnProperty("review_id", expect.any(Number))
+          ).toBe(true);
+          expect(
+            review.hasOwnProperty("category", expect.any(Number))
+          ).toBe(true);
+          expect(
+            review.hasOwnProperty("review_img_url", expect.any(Number))
+          ).toBe(true);
+          expect(
+            review.hasOwnProperty("created_at", expect.any(Number))
+          ).toBe(true);
+                  expect(
+            review.hasOwnProperty("votes", expect.any(Number))
+          ).toBe(true);
+                  expect(
+            review.hasOwnProperty("designer", expect.any(Number))
+          ).toBe(true);
+                  expect(
+            review.hasOwnProperty("comment_count", expect.any(Number))
+          ).toBe(true);
+        })
+      })
+    })
   })
 })
