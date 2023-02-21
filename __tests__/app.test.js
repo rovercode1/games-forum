aedfd04086c8091a9fe5f920db30ab2aeaf9760b
Hayden <haydenworden@outlook.com>
2023-02-21 11:20:28 +0000

    Tests added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,4 +41,88 @@ describe("api", () => {
         });
     });
   });
+
+  describe("/api/categories", () => {
+    it("200 - response ok status", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body }) => {
+          const categories = body.categories;
+
+          expect(categories.length).toBe(4);
+          categories.forEach((category) => {
+            expect(category.hasOwnProperty("slug", expect.any(String))).toBe(
+              true
+            );
+            expect(
+              category.hasOwnProperty("description", expect.any(String))
+            ).toBe(true);
+          });
+        });
+    });
+  });
+
+  // Responds with:
+
+  // a review object, which should have the following properties:
+
+  describe("/api/reviews/review_id", () => {
+    it.only("200 - responds with review object", () => {
+      return request(app)
+        .get("/api/reviews/2")
+        .expect(200)
+        .then(({body}) => {
+          expect(body.length).toBe(1);
+          const review = body[0];
+          //Should be an Object
+
+          expect(review.hasOwnProperty("review_id", expect.any(Number))).toBe(
+            true
+          );
+          expect(review.hasOwnProperty("title", expect.any(String))).toBe(true);
+          expect(review.hasOwnProperty("review_body", expect.any(String))).toBe(
+            true
+          );
+          expect(review.hasOwnProperty("designer", expect.any(String))).toBe(
+            true
+          );
+          expect(
+            review.hasOwnProperty("review_img_url", expect.any(String))
+          ).toBe(true);
+          expect(review.hasOwnProperty("votes", expect.any(Number))).toBe(true);
+          expect(review.hasOwnProperty("category", expect.any(String))).toBe(
+            true
+          );
+          expect(review.hasOwnProperty("owner", expect.any(String))).toBe(true);
+          expect(review.hasOwnProperty("created_at", expect.any(String))).toBe(
+            true
+          );
+        });
+    });
+
+    //     Bad review_id (e.g. /dog)
+    // Well formed review_id that doesn't exist in the database (e.g. /999999)
+
+    it("400 - responds with msg bad request", () => {
+      return request(app)
+        .get("/api/review/bad-request")
+        .expect(400)
+        .then(({ body }) => {
+          console.log(body)
+          const serverResponseMsg = body.msg;
+          expect(serverResponseMsg).toBe("Bad request");
+        });
+    });
+
+    it("404 - responds with msg when sent valid but non-existent path", () => {
+      return request(app)
+        .get("/api/review/99999999")
+        .expect(404)
+        .then(({ body }) => {
+          const serverResponseMsg = body.msg;
+          expect(serverResponseMsg).toBe("Path not found");
+        });
+    });
+  });
 });
