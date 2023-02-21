8f0fdc511a143dce55ada464ac4ac19e254ec46b
Hayden <haydenworden@outlook.com>
2023-02-21 15:12:01 +0000

    Error handling added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const { getCategories } = require("./controllers/forum-controllers");
+const { getCategories, getReviewById} = require("./controllers/forum-controllers");
 const {
   handleServerErrors,
   handle404Errors,
+  handle400Errors
 } = require("./controllers/error-handling");
 
 app.get("/api/categories", getCategories);
 
 app.use(handle404Errors);
+
+app.use(handle400Errors)
 app.use(handleServerErrors);
 module.exports = app;
--- controllers/error-handling.js
+++ controllers/error-handling.js
@@ -2,6 +2,10 @@ exports.handle404Errors = (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
 };
 
+exports.handle400Errors = ( req, res, next) => {
+  res.status(400).send({ msg: "Bad request" });
+};
+
 exports.handleServerErrors = (err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "Internal Server Error" });
