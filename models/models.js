f3f37940a3154f0c8f0d3e6a20c7ee37635bcb5b
Hayden <haydenworden@outlook.com>
2023-02-21 15:23:38 +0000

    Refactored code + changed review query

--- models/models.js
+++ models/models.js
@@ -2,36 +2,14 @@ const db = require("./../db/connection.js");
 const format = require("pg-format");
 exports.selectReviews = () => {
   return db
-    .query("SELECT * FROM reviews ORDER BY created_at DESC")
+    .query(`SELECT reviews.title, reviews.owner , reviews.review_id, reviews.category, review_img_url, reviews.review_body, reviews.created_at, reviews.votes, reviews.designer, COUNT(comment_id) as comment_count
+    FROM reviews
+    LEFT JOIN comments ON reviews.review_id = comments.review_id 
+    GROUP BY reviews.review_id
+    ORDER BY created_at DESC
+    `)
     .then((reviews) => {
       return reviews.rows;
     })
-    .then((reviews) => {
-      const dbQuery = `SELECT
-      review_id,
-      COUNT(*)
-      FROM
-      comments
-      GROUP BY
-      review_id;
-      `;
 
-      return Promise.all([db.query(dbQuery), reviews]);
-    })
-    .then((promiseArray) => {
-      const [comment_count, reviews] = [promiseArray[0].rows, promiseArray[1]];
-
-      const updatedReviews = reviews.map((review) => {
-        review.comment_count = 0;
-
-        for (let i = 0; i < comment_count.length; i++) {
-          if (review.review_id === comment_count[i].review_id) {
-            review.comment_count = comment_count[i].count;
-          }
-        }
-        return review;
-      });
-
-      return updatedReviews
-    });
 };
--- playground.sql
+++ playground.sql
@@ -1,13 +1,5 @@
 \c nc_games_test
--- SELECT COUNT(*) as comment_count
--- FROM comments 
-
--- WHERE review_id = 4
-
-SELECT
-	review_id,
-	COUNT(*)
-FROM
-	comments
-GROUP BY
-	review_id;
+SELECT reviews.title, reviews.owner , reviews.review_id, reviews.category, review_img_url, reviews.review_body, reviews.created_at, reviews.votes, reviews.designer, COUNT(comment_id) as comment_count
+FROM reviews
+LEFT JOIN comments ON reviews.review_id = comments.review_id 
+GROUP BY reviews.review_id
