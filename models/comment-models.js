b436e4dd0c0783c9e4df50fe380f54d816bbd887
Hayden <haydenworden@outlook.com>
2023-02-23 13:58:50 +0000

    200 GET happy path for reviews with no comments

--- models/comment-models.js
+++ models/comment-models.js
@@ -2,21 +2,31 @@ const db = require("./../db/connection.js");
 const format = require("pg-format");
 
 exports.selectComments = (reviewId) => {
-  let queryStr = 'SELECT * FROM comments'
-  let queryParam = []
+  let queryStr = `
+  SELECT reviews.review_id, comments.comment_id,  comments.votes, comments.created_at, comments.author, comments.body, COUNT(comment_id) as comment_count
+    FROM reviews
+    LEFT JOIN comments ON reviews.review_id = comments.review_id 
+  `;
+  let queryParam = [];
 
-  if(reviewId !== undefined){
-    queryStr+= ' WHERE review_id = $1 ORDER BY created_at DESC'
-    queryParam.push(reviewId)
-
+  if (reviewId !== undefined) {
+    queryStr += `
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.review_id, comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body
+    ORDER BY created_at DESC
+    `;
+    queryParam.push(reviewId);
   }
-  
-  return db
-  .query(queryStr, queryParam)
-  .then((comments)=>{
-    if(comments.rowCount === 0){
-      return Promise.reject('Comments not found.')
+
+  return db.query(queryStr, queryParam).then((comments) => {
+    if (comments.rowCount === 0) {
+      return Promise.reject("Comments not found.");
     }
-    return comments.rows
-  })
-}
+    const comment_count = comments.rows[0].comment_count;
+
+    if (+comment_count === 0) {
+      return [];
+    }
+    return comments.rows;
+  });
+};
