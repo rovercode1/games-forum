7926e5ed9d7058498f29732e2d90e8ba67003c83
Hayden <haydenworden@outlook.com>
2023-02-24 15:30:33 +0000

    tests and logic updated for comment_count

--- models/review-models.js
+++ models/review-models.js
@@ -16,11 +16,19 @@ exports.selectReviews = () => {
 };
 
 exports.selectReviewById = (reviewId) => {
-  let queryString = "SELECT * FROM reviews";
+  let queryString = `
+  SELECT reviews.title, reviews.owner , reviews.review_id, reviews.category, review_img_url, reviews.review_body, reviews.created_at, reviews.votes, reviews.designer, COUNT(comment_id) as comment_count
+  FROM reviews
+  LEFT JOIN comments ON reviews.review_id = comments.review_id 
+  `;
   let queryParam = [];
 
   if (reviewId !== undefined) {
-    queryString += " WHERE review_id = $1";
+    queryString += ` 
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.review_id
+    ORDER BY created_at DESC
+    `;
     queryParam.push(reviewId);
   }
   return db.query(queryString, queryParam).then((review) => {
